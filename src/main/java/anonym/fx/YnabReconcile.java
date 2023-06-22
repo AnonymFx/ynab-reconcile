6740155e5a27cf6d5e24dfa0f6b334dc6a17b1fd
Thomas Pettinger <pettinger.thomas@gmail.com>
2023-06-22 13:27:09 +0200

    Refactor DkbTransaction to BankTransaction

--- src/main/java/anonym/fx/YnabReconcile.java
+++ src/main/java/anonym/fx/YnabReconcile.java
@@ -5,7 +5,6 @@ import picocli.CommandLine;
 
 import java.io.File;
 import java.io.FileReader;
-import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.Callable;
 
@@ -16,7 +15,7 @@ public class YnabReconcile implements Callable<Integer> {
     @CommandLine.Parameters(index = "0", description = "The CSV file exported from YNAB (c.f. https://support.ynab.com/en_us/how-to-export-budget-data-Sy_CouWA9)")
     private File ynabCsv;
 
-    @CommandLine.Parameters(index = "1", description = "The CSV file exported from DKB")
+    @CommandLine.Parameters(index = "1", description = "The CSV file exported from your bank")
     private File dkbCsv;
 
     public static void main(String[] args) {
@@ -26,14 +25,17 @@ public class YnabReconcile implements Callable<Integer> {
 
     @Override
     public Integer call() throws Exception {
-        List<YnabTransaction> ynabTransactions = Collections.emptyList();
-        List<DkbTransaction> dkbTransactions = Collections.emptyList();
+        List<YnabTransaction> ynabTransactions;
+        List<BankTransaction> bankTransactions;
         try (FileReader fileReader = new FileReader(ynabCsv)) {
             ynabTransactions = new CsvToBeanBuilder<YnabTransaction>(fileReader).withType(YnabTransaction.class).build().parse();
         }
         try (FileReader fileReader = new FileReader(dkbCsv)) {
-            dkbTransactions = new CsvToBeanBuilder<DkbTransaction>(fileReader).withType(DkbTransaction.class).withSkipLines(4).withSeparator(';').build().parse();
+            bankTransactions = new CsvToBeanBuilder<BankTransaction>(fileReader).withType(BankTransaction.class).withSkipLines(4).withSeparator(';').build().parse();
         }
+
+        ynabTransactions.forEach(System.out::println);
+        bankTransactions.forEach(System.out::println);
 
         return 0;
     }
